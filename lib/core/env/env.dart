e59c7d6ca95a50c3f42e8236580b614575d1dd20
Nitva <valenit@proton.me>
2026-03-30 14:41:19 +0200

    hiden apikey

--- lib/core/env/env.dart
+++ lib/core/env/env.dart
@@ -2,9 +2,8 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied(path: '.env') // 1. Cambia @envied por @Envied (Mayúscula)
+@Envied(path: '.env')
 abstract class Env {
-
-    @EnviedField(varName: 'GTFS_KEY', obfuscate: true)
-    static final String apiKey = _Env.apiKey; // Al usar obfuscate, debe ser 'static final'
+  @EnviedField(varName: 'GTFS_KEY', obfuscate: true)
+  static final String apiKey = _Env.apiKey;
 }
